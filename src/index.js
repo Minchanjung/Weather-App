f1dc5710a69290e4bf08a3c1b37066a0f9abc4f2
Minchan Jung <minchanjung1@gmail.com>
2022-03-07 16:30:42 +0000

    form js working

--- src/index.js
+++ src/index.js
@@ -4,28 +4,37 @@ const apiKey = "0085e2529c794fe54e1b0c3f0f266a4f"
 let weatherData;
 
 const getWeatherData = async function(location) {
-    try {
-        const call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`, {mode: 'cors'});
-        console.log(call);
-        data = await call.json();
-        console.log(data);
-        weatherData = data.main;
-        console.log(weatherData);
-        return weatherData;
-
-    } catch(error) {
-        console.log(error);
-        console.log("hi");
-    };
+    const call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`, {mode: 'cors'})
+    .then(async (data) => {
+        if (data.ok) {
+            data = await data.json()
+            console.log(data);
+            weatherData = {'main': data.main, 'weather': data.weather};
+            console.log(weatherData);
+            return weatherData;
+        } else {
+            throw new Error(data.status + " Failed Fetch ");
+        }
+    }).catch(e => {
+        errorMsg();
+    })
 }
 
 const formInput = function() {
     const form = document.querySelector("form");
     const input = document.querySelector("#location");
+    const msg = document.querySelector("#errorMessage");
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         getWeatherData(input.value);
+        form.reset();
+        msg.style.display = "none";
     })
 };
 
 formInput();
+
+const errorMsg = function() {
+    const msg = document.querySelector("#errorMessage");
+    msg.style.display = "block";
+}
